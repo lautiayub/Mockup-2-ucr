13c3c8b940190ac6b2d770185e42d3f6ee1ccf73
Lautaro Ayub <lautiayub@hotmail.com>
2023-02-06 00:35:00 -0300

    Emprolijamiento de codigo css, e inicio de html seccion login

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 
-const menu=document.querySelector("#menu_logo_nav")
-const asideMenu =document.querySelector("#aside-mobile-left")
+const menu=document.querySelector(".menu_logo_nav")
+const asideMenu =document.querySelector(".aside-mobile-left")
 
-const objLi= document.querySelector("#obj-li-nav")
-const memLi = document.querySelector("#mem-li-nav")
-const tripLI=document.querySelector("#trip-li-nav")
+const objLi= document.querySelector(".obj-li-nav")
+const memLi = document.querySelector(".mem-li-nav")
+const tripLI=document.querySelector(".trip-li-nav")
 
-const objetives= document.querySelector("#objectives--nav")
-const membership= document.querySelector("#membership--nav")
-const trip= document.querySelector("#trip--nav")
+const objetives= document.querySelector(".objectives--nav")
+const membership= document.querySelector(".membership--nav")
+const trip= document.querySelector(".trip--nav")
 
 
 
 menu.addEventListener("click",openMenuAside);
+
 objLi.addEventListener("mouseover",oppenInfoOBJ);
 objLi.addEventListener("mouseout",closeInfoOBJ);
 
@@ -58,11 +59,11 @@ window.onscroll = function(){
     closeMenuAside()
     let desplazamiento = window.pageYOffset;
     if (ubicacionPrincipal>=desplazamiento){
-        document.getElementById("navbar").style.top="0";
+        document.querySelector(".navbar").style.top="0";
         
     }
     else{
-        document.getElementById("navbar").style.top="-50px"
+        document.querySelector(".navbar").style.top="-55px"
         
     }
     ubicacionPrincipal=desplazamiento;
